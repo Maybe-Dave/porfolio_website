452eb043238d1fbadbab666634fbcc774f0eca1c
David Dickson <dicksondave2002@gmail.com>
2025-02-22 10:11:34 +0100

    Updated Site partially

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+const themeSwitch = document.getElementById("icon")
+
+themeSwitch.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+    
+    if(document.body.classList.contains("dark")) {
+        themeSwitch.src = "/assets/sun.png"
+    }else{
+        themeSwitch.src = "/assets/moon.png"
+    }
+})
